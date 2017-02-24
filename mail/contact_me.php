ce2f3abb4d0319a312d662b0f9f8c9dfe16cbbc5
vitor.porto <vitorfwt@gmail.com>
2017-02-23 21:18:36 -0300

    ae

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,51 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'gabrielabessal@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Contato do portifolio:  $name";
-$email_body = "Você recebeu uma nova mensagem do formulario de contato no portifolio.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nMensagem:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+if(!isset($_POST[Submit])) die("N&atilde;o recebi nenhum par&acitc;metro. Por favor volte ao formulario.html antes");
+/* Medida preventiva para evitar que outros dom�nios sejam remetente da sua mensagem. */
+if (eregi('tempsite.ws$|locaweb.com.br$|hospedagemdesites.ws$|websiteseguro.com$', $_SERVER[HTTP_HOST])) {
+        $emailsender='seu@e-mail.com.br';
+} else {
+        $emailsender = "webmaster@" . $_SERVER[HTTP_HOST];
+        //    Na linha acima estamos for�ando que o remetente seja 'webmaster@seudominio',
+        // voc� pode alterar para que o remetente seja, por exemplo, 'contato@seudominio'.
+}
+ 
+/* Verifica qual � o sistema operacional do servidor para ajustar o cabe�alho de forma correta. N�o alterar */
+if(PHP_OS == "Linux") $quebra_linha = "\n"; //Se for Linux
+elseif(PHP_OS == "WINNT") $quebra_linha = "\r\n"; // Se for Windows
+else die("Este script nao esta preparado para funcionar com o sistema operacional de seu servidor");
+ 
+// Passando os dados obtidos pelo formul�rio para as vari�veis abaixo
+$nomeremetente     = strip_tags(htmlspecialchars($_POST['name']));
+$emailremetente    = strip_tags(htmlspecialchars($_POST['email']));
+$emaildestinatario = 'gabrielabessal@gmail.com';
+$comcopia          = 'vitorfwt@gmail.com';
+$comcopiaoculta    = '';
+$assunto           = 'Portifólio';
+$mensagem          = strip_tags(htmlspecialchars($_POST['message']));;
+ 
+ 
+/* Montando a mensagem a ser enviada no corpo do e-mail. */
+$mensagemHTML = '<P>Esta é a sua mensagem:</P>
+<p><b><i>'.$mensagem.'</i></b></p>
+<hr>';
+ 
+ 
+/* Montando o cabe�alho da mensagem */
+$headers = "MIME-Version: 1.1".$quebra_linha;
+$headers .= "Content-type: text/html; charset=iso-8859-1".$quebra_linha;
+// Perceba que a linha acima cont�m "text/html", sem essa linha, a mensagem n�o chegar� formatada.
+$headers .= "From: ".$emailsender.$quebra_linha;
+$headers .= "Return-Path: " . $emailsender . $quebra_linha;
+// Esses dois "if's" abaixo s�o porque o Postfix obriga que se um cabe�alho for especificado, dever� haver um valor.
+// Se n�o houver um valor, o item n�o dever� ser especificado.
+if(strlen($comcopia) > 0) $headers .= "Cc: ".$comcopia.$quebra_linha;
+if(strlen($comcopiaoculta) > 0) $headers .= "Bcc: ".$comcopiaoculta.$quebra_linha;
+$headers .= "Reply-To: ".$emailremetente.$quebra_linha;
+// Note que o e-mail do remetente ser� usado no campo Reply-To (Responder Para)
+ 
+/* Enviando a mensagem */
+mail($emaildestinatario, $assunto, $mensagemHTML, $headers, "-r". $emailsender);
+ 
+/* Mostrando na tela as informa��es enviadas por e-mail */
+return true;
 ?>
